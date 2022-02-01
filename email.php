852aca7cd7bfe10a642f825795b7c2ba767a420a
Luca Souza <89982608+LucasKhi@users.noreply.github.com>
2022-02-01 00:37:48 -0300

    feito o grosso

--- email.php
+++ email.php
@@ -1,45 +1,56 @@
 <?php
-date_default_timezone_set('America/Sao_Paulo');
-
-require_once('src/PHPMailer.php');
-require_once('src/SMTP.php');
-require_once('src/Exception.php');
-
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
 
-if((isset($_POST['email']) && !empty(trim($_POST['email']))) && (isset($_POST['mensagem']) && !empty(trim($_POST['mensagem'])))) {
+require_once 'phpmailer/Exception.php';
+require_once 'phpmailer/PHPMailer.php';
+require_once 'phpmailer/SMTP.php';
 
-	$nome = !empty($_POST['nome']) ? $_POST['nome'] : 'Não informado';
-	$email = $_POST['email'];
-	$assunto = !empty($_POST['assunto']) ? utf8_decode($_POST['assunto']) : 'Não informado';
-	$mensagem = $_POST['mensagem'];
-	$data = date('d/m/Y H:i:s');
+$mail = new PHPMailer(true);
+$mail->CharSet = 'UTF-8';
 
-	$mail = new PHPMailer();
-	$mail->isSMTP();
-	$mail->Host = 'smtp.gmail.com';
-	$mail->SMTPAuth = true;
-	$mail->Username = 'khi.lucass@gmail.com';
-	$mail->Password = '9982361kKHI!1810';
-	$mail->Port = 587;
- 
-	$mail->setFrom('khi.lucass@gmail.com');
-	$mail->addAddress('khi.lucass@gmail.com');
 
-	$mail->isHTML(true);
-	$mail->Subject = $assunto;
-	$mail->Body = "Nome: {$nome}<br>;
-				   Email: {$email}<br>;
-				   Mensagem: {$mensagem}<br>;
-				   Data/hora: {$data}<br>";
+if(isset($_POST['submit'])){
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $messagem = $_POST['mensagem'];
 
-	if($mail->send()) {
-		echo 'Email enviado com sucesso.';
-	} else {
-		echo 'Email não enviado.';
-	}
-} else {
-	echo 'Não enviado: informar o email e a mensagem.';
+  try{
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'khi.lucass@gmail.com';
+    $mail->Password = '9982361kKHI!1810'; 
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = '587';
+
+    $mail->setFrom('khi.lucass@gmail.com'); 
+    $mail->addAddress('luukarai@gmail.com');
+
+    
+    $mail->isHTML(true);
+    $mail->Subject = 'Contato de portfólio';
+    $mail->Body = "<h3>Nome : $nome <br>Email: $email <br>Mensagem : $messagem</h3>";
+
+    $mail->send();
+
+    $mail->ClearAddresses();
+    $mail->addAddress($email);
+    $mail->isHTML(true);
+    $mail->Subject    = 'Confirmação de contato';
+    $mail->Body       = '<div>
+                          <h2>Obrigado por entrar em contato</h2><br/>
+                          <p>Seu email foi enviado certinho, estarei retornando em breve.</p>
+                        </div>';
+    $mail->AltBody    = 'Seu email foi enviado certinho, estarei retornando em breve.';
+    $mail->Send();
+
+
+  } catch (Exception $e){
+    $alert = '<div class="alerta-erro">
+                <span>'.$e->getMessage().'</span>
+              </div>';
+  }
+  
+
 }
+?>
